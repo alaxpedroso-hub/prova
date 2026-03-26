7ea5e64343f4923696f407eee0ff4e618041de68
PEDROSO <alax.pedroso@escola.pr.gov.br>
2026-03-26 09:35:40 -0300

    prova alax

---
+++ ex02.js
@@ -0,0 +1,6 @@
+let num1 = 4;
+let num2 = 5;
+let multiplicação = num1 * num2;
+let soma = num1 + num2;
+let subtração = num1 - num2;
+console.log("soma ${soma} subtração ${subtração} mutiplicação ${multiplicação}");
---
+++ ex03.js
@@ -0,0 +1,6 @@
+let = 19; 
+if (idade => 18){
+    console.log ("maior de idade")
+}else{
+    console.log("menor de idade")
+}
